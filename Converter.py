73b7a8217296d360940f00c34c328ad193b77657
tanaya25 <neptanaya25@gmail.com>
2019-04-02 22:31:00 +0900

    カナ->ローマ字の変換実装

--- Converter.py
+++ Converter.py
@@ -1,7 +1,52 @@
 # coding:utf-8
 
 class RomaKanaConverter:
-    def Roma2Kana(self):
+    # mode: "kana" or "roma"
+    def __init__(self, text, mode):
+        # set kana or roma mode
+        if mode == "kana":
+            self.kana_text = text
+        elif mode == "roma":
+            self.roma_text = text
+        else:
+            print("set mode correctly")
+
+        self.create_dict()
+        
+    def create_dict(self):
+        kana_roma_dict = {}
+        roma_kana_dict = {}
+        # set dict for conversion
+        with open("kana_roma.csv", "r", encoding="utf-8") as f:
+            l_strip = [s.strip() for s in f.readlines()]
+        for line in l_strip:
+            (roma, _, kana) = line.split(",")
+            kana_roma_dict[kana] = roma
+            roma_kana_dict[roma] = kana
+        self.roma_kana_dict = roma_kana_dict
+        self.kana_roma_dict = kana_roma_dict
+
+    def tokenize_kana(self):
+        kana_token_list = []
+        [kana_token_list.append(item) for item in self.kana_text]
+        return kana_token_list
+
+    def tokenize_roma(self):
         pass
-    def Kana2Roma(self):
+
+    def roma2kana(self):
         pass
+
+    def kana2roma(self):
+        self.roma_text = ""
+        kana_token_list = self.tokenize_kana()
+        for token in kana_token_list:
+            self.roma_text += self.kana_roma_dict[token]
+        print(self.roma_text)
+
+if __name__ == "__main__":
+    text = "コンニチハ"
+    print(text)
+    print("↓")
+    RKC = RomaKanaConverter(text, mode="kana")
+    RKC.kana2roma()
--- aru-henkan.py
+++ aru-henkan.py
@@ -3,31 +3,36 @@ import sys
 import MeCab
 from Converter import RomaKanaConverter as RKC
 
-def tokenize_wakati(string):
-    # 分かち書き
-    tagger = MeCab.Tagger("-Owakati")
+class AruHenkan():
+    def __init__(self, text):
+        self.text = text
 
-    # 空白で分割 リストの末尾に\nが入るので削除        
-    token_list = tagger.parse(text).split(" ")[:-1]
-    return token_list
+    def tokenize_wakati(self, text):
+        # 分かち書き
+        tagger = MeCab.Tagger("-Owakati")
 
-def parse_chasen(string):
-    tagger = MeCab.Tagger("-Ochasen")
-    result = tagger.parse(string).split("\t")[:-2]
-    # 形態素解析の結果は
-    # 君      キミ    君      名詞-代名詞-一般
-    # カナと品詞だけ抜き出して返す
-    kana_lex_list = [result[1], result[3]]
-    return kana_lex_list
+        # 空白で分割 リストの末尾に\nが入るので削除        
+        token_list = tagger.parse(text).split(" ")[:-1]
+        return token_list
 
-def aru_henkan(string):
-    # 形態素解析を分かち書きごとに行う
-    token_list = tokenize_wakati(string)
-    kana_lex_list = [parse_chasen(token) for token in token_list]
-    return kana_lex_list
+    def parse_chasen(self, text):
+        tagger = MeCab.Tagger("-Ochasen")
+        result = tagger.parse(text).split("\t")[:-2]
+        # 形態素解析の結果は
+        # 君      キミ    君      名詞-代名詞-一般
+        # カナと品詞だけ抜き出して返す
+        kana_lex_list = [result[1], result[3]]
+        return kana_lex_list
+
+    def aru_henkan(self):
+        # 形態素解析を分かち書きごとに行う
+        token_list = self.tokenize_wakati(self.text)
+        kana_lex_list = [self.parse_chasen(token) for token in token_list]
+        return kana_lex_list
 
 if __name__ == '__main__':
     # TODO:ここを標準入力にする
     text = 'オタクはモリモリと飯を食え'
-    result = aru_henkan(text)
+    AH = AruHenkan(text)
+    result = AH.aru_henkan()
     [print(item) for item in result]
